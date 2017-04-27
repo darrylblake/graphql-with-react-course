ac0256b8e113599c56483bb1ec63237f70a389fa
Darryl Blake <darryl.blake@orionhealth.com>
2017-04-27 17:03:31 +1200

    Created simple SongList component bonded with gql query

---
+++ lyrical-graphql/client/components/SongList.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+class SongList extends Component {
+  renderSongs() {
+    const { songs = [] } = this.props.data;
+
+    return songs.map(song => (
+      <li key={song.id} className="collection-item">{song.title}</li>
+    ));
+  }
+
+  render() {
+    const { loading } = this.props.data;
+    if (loading) return <div>Loading...</div>;
+    return (
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
+    );
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
