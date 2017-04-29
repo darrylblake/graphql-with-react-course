750f2465872f5e21b32ec0e03fafbd58845f4e80
Darryl Blake <darryl@darrylblake.com>
2017-04-30 07:41:51 +1200

    Handle deleting songs from list.
    - Another way to refetch queries: props.data.refetch()

--- lyrical-graphql/client/components/SongList.js
+++ lyrical-graphql/client/components/SongList.js
@@ -2,13 +2,32 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
 import query from "../queries/fetchSongs";
+import gql from "graphql-tag";
 
 class SongList extends Component {
+  handleDelete(id) {
+    const deleted = this.props
+      .mutate({
+        variables: {
+          id
+        }
+      })
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
     const { songs = [] } = this.props.data;
 
-    return songs.map(song => (
-      <li key={song.id} className="collection-item">{song.title}</li>
+    return songs.map(({ id, title }) => (
+      <li key={id} className="collection-item">
+        {title}
+        <i
+          onClick={() => this.handleDelete.bind(this)(id)}
+          className="material-icons right"
+        >
+          delete
+        </i>
+      </li>
     ));
   }
 
@@ -29,4 +48,12 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(graphql(query)(SongList));
