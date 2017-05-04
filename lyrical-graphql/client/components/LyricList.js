c2377ba65f0c0710f21129bb7524bf1984fcecce
Darryl Blake <darryl@darrylblake.com>
2017-05-04 15:09:53 +1200

    Submits and fetches likes

--- lyrical-graphql/client/components/LyricList.js
+++ lyrical-graphql/client/components/LyricList.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
 
 class LyricList extends Component {
+  handleLike(id) {
+    this.props.mutate({
+      variables: {
+        id
+      }
+    });
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
-      return <li className="collection-item" key={id}>{content}</li>;
+    return this.props.lyrics.map(({ id, content, likes }) => {
+      return (
+        <li className="collection-item" key={id}>
+          {content}
+          <i
+            className="material-icons right"
+            onClick={() => this.handleLike(id)}
+          >
+            thumb_up
+          </i>
+          {likes > 0 &&
+            <span className="badge" data-badge-caption="likes">{likes}</span>}
+        </li>
+      );
     });
   }
 
@@ -16,4 +38,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes    
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
