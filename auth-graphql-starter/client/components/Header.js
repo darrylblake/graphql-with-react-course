df22a9d93f90affa2f772035a2a2a55f456f2508
Darryl Blake <darryl@darrylblake.com>
2017-05-05 19:59:43 +1200

    Renders headers with buttons handles logout mutation

--- auth-graphql-starter/client/components/Header.js
+++ auth-graphql-starter/client/components/Header.js
@@ -1,9 +1,52 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { Link } from "react-router";
+import query from "../queries/CurrentUser";
+import mutation from "../mutations/Logout";
 
 class Header extends Component {
+  handleLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }]
+    });
+  }
+
+  renderButtons() {
+    const { loading, user } = this.props.data;
+
+    if (loading) return null;
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.handleLogoutClick.bind(this)}>Logout</a>
+        </li>
+      );
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">Home</Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+export default graphql(mutation)(graphql(query)(Header));
